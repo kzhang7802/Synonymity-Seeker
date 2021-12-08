3d0c8aab918801ef46f6c507625f401197a91e15
Jeremy Garcia <73523692+jareuhmee@users.noreply.github.com>
2021-12-08 01:30:12 -0500

    Optimization by passing as references

--- Graph.cpp
+++ Graph.cpp
@@ -1,19 +1,19 @@
 #include "Graph.h"
 
-Graph::Vertex::Vertex(std::string name) {
+Graph::Vertex::Vertex(const std::string&  name) {
     this->name = name;
 }
 
-Graph::Vertex::Vertex(std::string name, std::string speech) {
+Graph::Vertex::Vertex(const std::string&  name, const std::string&  speech) {
     this->name = name;
     this->speech = speech;
 }
 
-void Graph::Vertex::addSynonyms(Graph::Vertex syn) {
+void Graph::Vertex::addSynonyms(const Graph::Vertex& syn) {
     synonyms.push_back(syn);
 }
 
-void Graph::Vertex::setSpeech(std::string speech) {
+void Graph::Vertex::setSpeech(const std::string&) {
     this->speech = speech;
 }
 
@@ -34,37 +34,29 @@ Graph::Graph(std::unordered_map<std::string, std::pair<std::string, std::vector<
         if (allWords.find(i.first) == allWords.end()) {
 
             // Create a new vertex and give it a name (i.first) and a part of speech (i.second.first)
-            Vertex newVertex(i.first, i.second.first);
-            // Insert the name into the set of words we've encountered
-            allWords.insert(i.first);
-            allVertices.insert({i.first, newVertex});
+            Vertex vertex = newVertex(i.first, i.second.first);
 
             // Iterating over the vector of synonyms of the word we're dealing with
-            for (auto j : i.second.second) {
+            for (const auto& j : i.second.second) {
 
                 // If the synonym has not been encountered before in the entire graph
                 // If the synonym cannot be found in the set
                 if (allWords.find(j) == allWords.end()) {
 
                     // Creates a new vertex and gives it just a name (j)
-                    Vertex newNeighbor(j);
-                    // Insert the name into the set of words we've encountered
-                    allWords.insert(j);
-                    allVertices.insert({j, newNeighbor});
-                    // Add the newly created vertex in the linked list of synonyms of the original word we were dealing with
-                    newVertex.addSynonyms(newNeighbor);
+                    Vertex neighbor = newNeighbor(j, vertex);
                     // Add the newly created vertex in the graph
-                    adjList.push_back(newNeighbor);
+                    adjList.push_back(neighbor);
                 }
                 else {
                     // Otherwise, if the word has already been encountered (and a vertex was thus created for the word)
                     // Simply add it into the linked list of synonyms of the original word we were dealing with
-                    newVertex.addSynonyms(findVertex(j));
+                    vertex.addSynonyms(findVertex(j));
                 }
             }
 
             // Add the original word we were dealing with into our graph
-            adjList.push_back(newVertex);
+            adjList.push_back(vertex);
         }
             // If the key (word) has been encountered before
             // If the key can be found in the set
@@ -75,21 +67,16 @@ Graph::Graph(std::unordered_map<std::string, std::pair<std::string, std::vector<
             vertex.setSpeech(i.second.first);
 
             // Iterating over the vector of synonyms of the word we're dealing with
-            for (auto j : i.second.second) {
+            for (const auto& j : i.second.second) {
 
                 // If the synonym has not been encountered before in the entire graph
                 // If the synonym cannot be found in the set
                 if (allWords.find(j) == allWords.end()) {
 
                     // Creates a new vertex and gives it just a name (j)
-                    Vertex newNeighbor(j);
-                    // Insert the name into the set of words we've encountered
-                    allWords.insert(j);
-                    allVertices.insert({j, newNeighbor});
-                    // Add the newly created vertex in the linked list of synonyms of the original word we were dealing with
-                    vertex.addSynonyms(newNeighbor);
+                    Vertex neighbor = newNeighbor(j, vertex);
                     // Add the newly created vertex in the graph
-                    adjList.push_back(newNeighbor);
+                    adjList.push_back(neighbor);
                 }
 
                 else {
@@ -110,12 +97,12 @@ std::unordered_set<std::string> Graph::getWordSet() {
     return allWords;
 }
 
-Graph::Vertex Graph::findVertex(std::string nameSearch) {
+Graph::Vertex Graph::findVertex(const std::string& nameSearch) {
     return allVertices.find(nameSearch)->second;
 }
 
 // Helper function to find a particular vertex in a graph based on its name
-Graph::Vertex Graph::findVertexTraversal(std::string nameSearch) {
+Graph::Vertex Graph::findVertexTraversal(const std::string& nameSearch) {
     // Default vertex if a particular vertex isn't found for whatever reason
     // Essentially, it creates a vertex and gives it just the name of the vertex we were seeking
     Vertex vertex(nameSearch);
@@ -131,4 +118,29 @@ Graph::Vertex Graph::findVertexTraversal(std::string nameSearch) {
 
     // Otherwise, return the default vertex we created
     return vertex;
+}
+
+Graph::Vertex Graph::newVertex(const std::string& name, const std::string& syn) {
+    // Create a new vertex and give it a name and a part of speech
+    Vertex vertex(name, syn);
+    // Insert the name into the unordered_set of words we've encountered
+    allWords.insert(name);
+    // Insert the vertex into the unordered_map of vertices we've encountered
+    allVertices.insert({name, vertex});
+
+    return vertex;
+}
+
+Graph::Vertex Graph::newNeighbor(const std::string& name, Vertex& vertex) {
+    // Creates a new vertex and gives it just a name
+    Vertex newVertex(name);
+    // Insert the name into the unordered_set of words we've encountered
+    allWords.insert(name);
+    // Insert the vertex into the unordered_map of vertices we've encountered
+    allVertices.insert({name, newVertex});
+
+    // Add the newly created vertex in the linked list of synonyms of the original word we were dealing with
+    vertex.addSynonyms(newVertex);
+
+    return newVertex;
 }
--- Graph.h
+++ Graph.h
@@ -17,17 +17,17 @@ public:
         std::list<Vertex> synonyms;
     public:
         // Constructor for when the part of speech isn't known
-        Vertex(std::string name);
+        Vertex(const std::string& name);
 
         // Constructor for when the part of speech is known
         // This is the case when we're adding a vertex as a result of an original word, and not as a synonym of a word
-        Vertex(std::string name, std::string speech);
+        Vertex(const std::string& name, const std::string& speech);
 
         // Function to add a vertex to the linked list of synonyms for each vertex
-        void addSynonyms(Vertex syn);
+        void addSynonyms(const Vertex& syn);
 
         // Setter function for the part of speech
-        void setSpeech(std::string speech);
+        void setSpeech(const std::string& speech);
 
         // Getter function for the name
         std::string getName();
@@ -42,10 +42,10 @@ public:
     Graph(std::unordered_map<std::string, std::pair<std::string, std::vector<std::string>>> result);
 
     // Helper function to find a particular vertex in a graph based on its name
-    Vertex findVertex(std::string nameSearch);
+    Vertex findVertex(const std::string& nameSearch);
 
     // Helper function to find a particular vertex during traversal
-    Vertex findVertexTraversal(std::string nameSearch);
+    Vertex findVertexTraversal(const std::string& nameSearch);
 
     // Getter function for returning the adjacency list
     std::vector<Vertex> getAdjList();
@@ -57,8 +57,16 @@ private:
     // The vector essentially stores all the vertices in the graph
     // It's equivalent to the array in an adjacency list
     std::vector<Vertex> adjList;
+
     // The set keeps track of all the words we've encountered
     std::unordered_set<std::string> allWords;
+
     // A set of all vertices
     std::unordered_map<std::string, Vertex> allVertices;
+
+    // Creates new vertex (including name and synonym)
+    Vertex newVertex(const std::string& name, const std::string& syn);
+
+    // Creates new neighbor (including name and linking)
+    Vertex newNeighbor(const std::string& name, Vertex& vertex);
 };
