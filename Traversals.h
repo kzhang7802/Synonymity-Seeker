2097d82f8d07d2643156f6ed8438fe4a7de43bdf
officialkzhang <65622423+officialkzhang@users.noreply.github.com>
2021-12-07 23:35:41 -0500

    One revision to DFS, added edge counter for BFS
    Added check for DFS to ensure no infinite loops when a word without synonyms is met. Additionally, reconfigured BFS to return a vector of pairs, with each Vertex being appended a respective number of edges it's away from the source vertex (useful for algorithm).

--- Traversals.h
+++ Traversals.h
@@ -6,9 +6,9 @@
 #include <stack>
 #include "Graph.h"
 
-// Returns a vector of class Vertex objects synonymous to the user inputted word using BFS
-std::vector<Graph::Vertex> bfs(Graph graph, const std::string &word, const int size) {
-    std::vector<Graph::Vertex> result;
+// Returns a vector of pairs including Vertex objects and their respective distances from the source vertex (as integers), all synonymous to the user inputted word using BFS
+std::vector<std::pair<Graph::Vertex, int>> bfs(Graph graph, const std::string &word, const int size) {
+    std::vector<std::pair<Graph::Vertex, int>> result;
     std::queue<Graph::Vertex> vertexQ;
     std::unordered_set<std::string> visited;
 
@@ -19,6 +19,15 @@ std::vector<Graph::Vertex> bfs(Graph graph, const std::string &word, const int s
     vertexQ.push(current);
     visited.insert(current.getName());
 
+    // Counts the number of edges traversed from the source vertex
+    int edgesTraversed = 1;
+    // Counts the number of synonyms sharing the particular number of edges from the source vertex
+    int numSynonyms = 0;
+    // Keeps track of the next iteration of the number of synonyms sharing the particular number of edges from the source vertex
+    int nextNumSynonyms = 0;
+    // Boolean to keep track if the synonym is one edge away from the source vertex
+    bool firstTime = true;
+
     // While vertex queue is not empty
     while (!vertexQ.empty()) {
 
@@ -26,8 +35,25 @@ std::vector<Graph::Vertex> bfs(Graph graph, const std::string &word, const int s
         current = vertexQ.front();
         vertexQ.pop();
 
+        // Helper variable to store the list of synonyms
+        auto synonymList = current.getSynonyms();
+        // Size of the synonym list
+        numSynonyms = synonymList.size();
+
+        // If all the synonyms sharing the same number of edges traversed from the source vertex are exhausted
+        if (numSynonyms == 0) {
+            // The edges traversed from the source vertex must increase by one
+            edgesTraversed++;
+            // The saved next iteration of number of synonyms sharing the same number of edges to be traversed is replacing the previous counter
+            numSynonyms = nextNumSynonyms;
+            // Reset the next iteration
+            nextNumSynonyms = 0;
+        }
+        // If the synonyms are not exhausted, decrement its counter
+        numSynonyms--;
+
         // For all vertices, v, adjacent to his vertex, u...
-        for (auto &syn : current.getSynonyms())
+        for (auto &syn : synonymList) 
         {
             // If size of resulting vector is equivalent to specified size, return resulting vector
             if (result.size() == size)
@@ -36,12 +62,20 @@ std::vector<Graph::Vertex> bfs(Graph graph, const std::string &word, const int s
             // ...if v has not been visited...
             if (visited.find(syn.getName()) == visited.end())
             {
+                // Only increment the counter keeping track of the next iteration of synonyms if not pertaining to the synonyms of the source vector
+                if (!firstTime) {
+                    nextNumSynonyms++;
+                }
+
                 // ...mark this vertex v as identified and push into queue
-                result.push_back(syn);
+                result.push_back(std::make_pair(syn, edgesTraversed));
                 visited.insert(syn.getName());
                 vertexQ.push(syn);
             }
         }
+
+        // Keep the boolean as false for subsequent iterations
+        firstTime = false;
     }
     // Return resulting vector
     return result;
@@ -85,6 +119,11 @@ std::vector<Graph::Vertex> dfs(Graph graph, const std::string &word, const int s
             else
                 vertexStk.pop();
         }
+        // If the current vertex has no symptoms, pop it
+        if (current.getSynonyms().size() == 0) {
+            vertexStk.pop();
+        }
+
     }
     // Return resulting vector
     return result;
